8ba72d768cc1539a1017f16ba823e6097cf1de9c
codewithrahulpaswan <123496722+codewithrahulpaswan@users.noreply.github.com>
2023-08-07 18:20:45 +0500

    Add files via upload

---
+++ portfolio/script.js
@@ -0,0 +1,11 @@
+var typed = new Typed(".multiple-text", {
+    strings: ["Frontend Developer","UI Developer","Graphic Designer" ,"Blogger","YouTuber"],
+    typeSpeed: 100,
+    backspeed: 100,
+    backDelay: 1000,
+    loop: true
+})
+
+//mix it up portfolio sectio//
+
+var mixer = mixitup('.portfolio-gallery');
